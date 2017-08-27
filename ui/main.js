8c288be664a6a58ac7a8e0fc773dc79e8bb2f5ae
dsilejbdebt <30697253+dsilejbdebt@users.noreply.github.com>
2017-08-27 17:05:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,12 @@ var element= document.getElementById('main_text');
 element.innerHTML="new value";
 
 var img=document.getElementById('madi');
+var marginleft=0;
 funtion moveRight () {
     marginleft=marginleft+5;
     img.style.marginleft=marginleft+'px';
+}
+
+img.onclick= function(){
+    var interval=setInterval(moveRight,50);
 }
