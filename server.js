266cd566683c90da7a54d8fba8bcdf17f99d38b7
dsilejbdebt <30697253+dsilejbdebt@users.noreply.github.com>
2017-08-30 00:00:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,76 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles = {
+    'article-one': {
+                    title : 'Article One | dsi16',
+                    heading: 'Article One',
+                    date: '29/08/2017',
+                    content: `<p>
+                                This is my first article and the probably not the last.This is my first article and the probably not the last.
+                                This is my first article and the probably not the last.This is my first article and the probably not the last.
+                                This is my first article and the probably not the last.This is my first article and the probably not the last.
+                                
+                                </p>
+                                
+                                <p>
+                                This is my first article and the probably not the last.This is my first article and the probably not the last.
+                                This is my first article and the probably not the last.This is my first article and the probably not the last.
+                                This is my first article and the probably not the last.This is my first article and the probably not the last.
+                                </P>`
+
+        
+                    },
+    'article-two' :{
+                    title:'Article Two | dsi16',
+                    heading:'Article Two',
+                    date: '29/08/2017',
+                    content:` 
+            
+                            <p>
+                            This is my second article.
+                            </p>`
+    }
+}; 
+
+function createTemplate(data){
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+    
+    var htmlTemplate= `
+                <!doctype html>
+                <html>
+                <head>
+                    <title>${title}</title>
+                    <meta name="viewport" content="width=device-width, initail-scale=1"/>
+                    
+                    <link href="/ui/style.css" rel="stylesheet" />
+                    
+                </head>
+                <body>
+                    <div>
+                        <a href="/">Home</a>
+                    </div>
+                    
+                    <hr/>
+                    
+                    <div class=container>
+                    <h3>${heading}</h3>
+                    
+                    <div>
+                        ${date}
+                    </div>
+                    
+                    <div>
+                       ${content}
+                    </div>
+                    </div>
+                </body>
+                </html>`
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -21,8 +91,9 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/:articlename', function (req, res) {
+    var articlename= reg.param.articlename;
+  res.send(createTemplate(articlename));
 });
 
 
